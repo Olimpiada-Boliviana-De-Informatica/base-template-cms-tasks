980ad560b6cff72bc4b8435f61467ce3304a3289
Oscar Gauss Carvajal Yucra <dev@oscargauss.com>
2025-05-31 09:34:04 -0400

    fixing task scripts

--- scripts/task-gen-statement.sh
+++ scripts/task-gen-statement.sh
@@ -1,7 +1,7 @@
 gen_statement() {
   cd tasks || exit
 
-  python3 .commons/export_pdf.py "$1" "$1/statement" "$1/statement/es.pdf"
+  python3 .commons/export_pdf.py "$2" "$1/statement" "$1/statement/$3.pdf"
 }
 
-gen_statement $1
+gen_statement $1 $2 $3
--- scripts/task-gen-zip.sh
+++ scripts/task-gen-zip.sh
@@ -4,7 +4,23 @@ gen_zip() {
   for task_name in "$@"; do
     echo "Task: $task_name"
     cd "$task_name" || exit
-    tps export CMS 1 --statement-pdf statement/es.pdf
+#    tps export CMS 1 --statement-pdf statement/es.pdf
+    tps export CMS 1
+    # adding statements
+    file_zip="$(ls -1t *.zip | head -n 1)"
+    rm -rf "./$file_zip-tmp"
+    unzip "$file_zip" -d "./$file_zip-tmp"
+
+    first_subfolder=$(find "./$file_zip-tmp" -mindepth 1 -maxdepth 1 -type d | sort | head -n 1)
+    task_code=$(basename "$first_subfolder")
+
+    mkdir -p "./$file_zip-tmp/$task_code/statements"
+    cp statement/*.pdf "./$file_zip-tmp/$task_code/statements"
+    rm -rf "./$file_zip"
+    cd "./$file_zip-tmp"
+    zip -r "../$file_zip" "./$task_code/"
+    cd ..
+    rm -rf "./$file_zip-tmp"
   done
 }
 
